1dd86d98d04843a9e4e2e44cf334016c7ee3f889
tommaso.dare <tommaso.dare96@gmail.com>
2025-06-05 18:10:54 +0200

    Done Milestone 2

--- index.php
+++ index.php
@@ -4,22 +4,7 @@ $lunghezza = isset($_GET['lunghezza']) ? $_GET['lunghezza'] : 'null';
 
 var_dump($lunghezza);
 
-function randompassword($long)
-{
-
-    $password = "";
-    $possibleChars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_?*+&%!#@><°-:,/';
-
-    for ($i = 0; $i < $long; $i++) {
-
-        $randomsign = rand(0, strlen($possibleChars) - 1); // strlen è usato per ottenere la lunghezza della stringa
-        $password .= $possibleChars[$randomsign];
-
-        /*  var_dump($randomsign); */
-    }
-
-    return "<div class='alert alert-success mt-3'>La tua password sicura è: <strong>" . $password . "</strong></div>";
-}
+include_once './function.php'
 
 ?>
 
